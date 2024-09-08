6d22eb7b50cc054134f971bdf6b17c439aedacf4
mlryckman27 <mlryckman27@gmail.com>
2024-09-08 15:01:32 -0400

    Added color to shadow.

---
+++ BlazorWASM.Test/Shared/NavGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorWASM.Test.Shared
+{
+    public class NavGroup
+    {
+        public string Name { get; set; }
+        public List<NavLinkItem> Links { get; set; } = new List<NavLinkItem>();
+    }
+
+    public class NavLinkItem
+    {
+        public string Href { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ CoveKit/Contracts/IShadowable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoveKit
+{
+    internal interface IShadowable
+    {
+        bool Shadow { get; set; }
+        string ShadowColor { get; set; }
+        string ApplyShadow(string shadowColor);
+    }
+}
