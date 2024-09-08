203da0fe96da1ff9cc1cb27d373f6a311c3226cd
mlryckman27 <mlryckman27@gmail.com>
2024-09-07 21:38:23 -0400

    Created input component.

---
+++ CoveKit/Contracts/IAdjustableWidth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoveKit
+{
+    internal interface IAdjustableWidth
+    {
+        string SetWidth(int width);
+    }
+}
---
+++ CoveKit/Contracts/ICoveKitComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoveKit
+{
+    internal interface ICoveKitComponent
+    {
+        string? Class { get; set; }
+        string? Style { get; set; }
+    }
+}
