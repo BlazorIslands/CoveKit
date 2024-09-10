896f434d065ff31503641910fd6b6bb984a25659
mlryckman27 <mlryckman27@gmail.com>
2024-09-09 21:10:11 -0400

    Added card component.

---
+++ CoveKit/Contracts/IBackgroundable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoveKit
+{
+    internal interface IBackgroundable
+    {
+        CoveColor BackgroundColor { get; set; }
+        string SetBackgroundColor(CoveColor backgroundColor);
+    }
+}
